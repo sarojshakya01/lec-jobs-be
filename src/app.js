bc65d44bff6db84c7e71783d08e842732b1d4d3f
sarojshakya01 <sarojsh01@gmail.com>
2023-08-18 22:27:47 +0545

    fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require("express");
-
 // const fs = require("fs");
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
+
 const { router } = require("./routes");
 const { connectDB } = require("./db");
+const { PORT } = require("./config");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const PORT = 5001; // api port
 
 connectDB();
 
